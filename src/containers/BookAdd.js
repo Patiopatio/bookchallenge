0f98210bfc1c4eef4615502e33a7c522d089b28b
Patiopatio <buczkowska.p@gmail.com>
2019-04-23 15:09:36 +0200

    updated bookAdd, bookList

--- src/containers/BookAdd.js
+++ src/containers/BookAdd.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {FormGroup, Label, Input} from 'reactstrap';
+import {FormGroup, Label, Input, Row, Col} from 'reactstrap';
+import bookCover from "../images/book1.jpeg";
+
 
 class BookAdd extends Component {
   constructor(props) {
@@ -14,17 +16,14 @@ class BookAdd extends Component {
 
   handleChange = event => {
       const {name, value} = event.target
-
       this.setState ({
         bookToAdd: {[name]: value,}
 
       })
     }
-    submitForm = () => {
+    addBook = () => {
       console.log(this.state)
       this.props.history.push('/user')
-      // this.props.handleSubmit(this.state)
-      // this.setState(this.initialState)
     }
 
 
@@ -32,25 +31,41 @@ class BookAdd extends Component {
     return (
       <div>
         <h2>Book Add</h2>
-        <div>
-          <FormGroup>
-              <label>Title</label>
-                <input
+        <Row className='d-flex justify-content-center'>
+          <Col xs="3">
+          <img src={bookCover} className="img-rounded img-thumbnail" alt="Book Cover" />
+          </Col>
+          <Col xs="9">
+            <FormGroup>
+              <Label fro="title">Title</Label>
+              <Input
                 type="text"
-                name="Title"
+                name="title"
                 value={this.state.bookToAdd.title}
-                onChange={this.handleChange} />
-              <label>ISBN</label>
-                <input
+                onchange={this.handelChange}
+                placeholder="Enter the title here"
+                id="title" />
+            </FormGroup>
+            <FormGroup>
+              <Label fro="isbn">ISBN</Label>
+              <Input
                 type="text"
-                name="ISBN"
-                value={this.state.bookToAdd.ISBN}
-                onChange={this.handleChange} />
-                <input type="button" value="Submit" onClick={this.submitForm} />
-
-          </FormGroup>
-
-        </div>
+                name="isbn"
+                value={this.state.bookToAdd.isbn}
+                onchange={this.handelChange}
+                placeholder="978-1-56619-909-4 "
+                id="isbn" />
+            </FormGroup>
+            <input type="button"
+              value="Cancel"
+              className="btn btn-outline-danger mr-2"
+              onClick={this.goBack} />
+            <input type="button"
+              value="Add Book"
+              className="btn btn-outline-info"
+              onClick={this.addBook} />
+          </Col>
+        </Row>
       </div>
     )
   }
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,37 +1,44 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {Card, CardImg, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
+import bookCover from '../images/book1.jpeg';
 
 class BookList extends Component {
   constructor(props) {
     super(props)
     this.state= {
       books: [{
-        title: "bla",
-        ISBN: 1
+        title: "Harry Potter 1",
+        isbn: '978-3551557414'
       },
       {
-        title: "ble",
-        ISBN: 2
+        title: "Harry Potter 2",
+        isbn: '978-3551557421'
       },
       {
-        title: "blo",
-        ISBN: 3
+        title: "Harry Potter 3",
+        isbn: '978-3551557438'
       }],
-
-
     }
   }
-
 
     render() {
       return (
         <div>
           <h2>Books</h2>
-            {this.state.books.map((book) =>
-              <div>{book.title}  - {book.ISBN}
-                <Link to="/books/1">Book1</Link>
-              </div>
+          <div className="card-deck">
+            {this.state.books.map((book, index) =>
+              <Card key={index}>
+                <CardImg top width="100%" src={bookCover} alt="No cover available" />
+                <CardBody>
+                  <CardTitle classname="front-weight-bold">{book.title}</CardTitle>
+                  <CardSubtitle>ISBN: {book.isbn}
+                  </CardSubtitle>
+                  <Link to="/books/1">See Evaluations</Link>
+                </CardBody>
+              </Card>
             )}
+          </div>
         </div>
       )
     }
