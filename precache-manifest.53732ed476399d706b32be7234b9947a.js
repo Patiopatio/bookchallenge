ed17acf1261de9e1bdab5c300ae197b9bc8b7c47
Patiopatio <buczkowska.p@gmail.com>
2019-04-23 22:30:38 +0200

    Updates

---
+++ precache-manifest.53732ed476399d706b32be7234b9947a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ff5dc2ab128a80c45dcfbac6df0b3ec",
+    "url": "/Patiopatio/bookchallenge/static/media/book1.1ff5dc2a.jpeg"
+  },
+  {
+    "revision": "b05eb29b7f7f2897032f",
+    "url": "/Patiopatio/bookchallenge/static/js/runtime~main.a06ac625.js"
+  },
+  {
+    "revision": "e0d20ae0bfe119fd4e28",
+    "url": "/Patiopatio/bookchallenge/static/js/main.968b5481.chunk.js"
+  },
+  {
+    "revision": "f189b84570b32470abe0",
+    "url": "/Patiopatio/bookchallenge/static/js/2.31a710bd.chunk.js"
+  },
+  {
+    "revision": "e0d20ae0bfe119fd4e28",
+    "url": "/Patiopatio/bookchallenge/static/css/main.a69ceec3.chunk.css"
+  },
+  {
+    "revision": "f189b84570b32470abe0",
+    "url": "/Patiopatio/bookchallenge/static/css/2.e9d432e2.chunk.css"
+  },
+  {
+    "revision": "3730e1987533ec564e3aa3cfd936e2d2",
+    "url": "/Patiopatio/bookchallenge/index.html"
+  }
+];
