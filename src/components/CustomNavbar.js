c1c84130bcde065f0ab10ab74ac6924fc23b295f
Patiopatio <buczkowska.p@gmail.com>
2019-04-23 16:17:46 +0200

    updated customNavbar

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react'
-import {Navbar, NavbarBrand} from 'reactstrap';
+import {Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem, NavLink, UncontrolledDropdown,
+DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class CustomNavbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className="Navbar">
         <Navbar color="light" light expand="md">
           <NavbarBrand href="/">Book Challenge</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/books">Books</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/books/1">Book Details</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/bookAdd">Add Book</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/user">User</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/login">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/register">Register</NavLink>
+              </NavItem>
+              </Nav>
+          </Collapse>
         </Navbar>
       </div>
     )
--- src/containers/App.js
+++ src/containers/App.js
@@ -20,25 +20,6 @@ class App extends Component {
         <Container>
           <Router>
             <div>
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/books">Books</Link>
-                  </li>
-                  <li>
-                    <Link to="/books/1">Book Details</Link>
-                  </li>
-                  <li>
-                    <Link to="/user">User</Link>
-                  </li>
-                  <li>
-                    <Link to="/login">Login</Link>
-                  </li>
-                  <li>
-                    <Link to="/register">Register</Link>
-                  </li>
-                </ul>
-              </nav>
               <Switch>
                 <Route path="/books/1" component={BookDetails} />
                 <Route path="/books" component={BookList} />
@@ -47,7 +28,6 @@ class App extends Component {
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
                 <Route component={BookList} />
-
               </Switch>
             </div>
           </Router>
